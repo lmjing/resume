4ac7dbc28af79be9788aea7f864fc7c70e2b8b04
lmjing <lmjing4905@gmail.com>
2022-04-07 17:13:45 +0900

    스킬 뱃지로 변경

--- src/components/Skill.js
+++ src/components/Skill.js
@@ -1,22 +1,34 @@
+import './Skill.scss'
 import Title from "./Title";
 
 function Skill(props) {
     const {data} = props;
 
+    const skillBadges = (skills) => {
+        return (
+            skills.map(skill => {
+                const {name, color, logoColor} = skill
+                return <img className="badge" alt={name} key={name}
+                            src={`https://img.shields.io/badge/${name}-${color}?style=for-the-badge&logo=${name}&logoColor=${logoColor}`}/>
+            })
+        )
+    }
+
     return (
         <section>
             <Title text="Skills"/>
-            <ul>
-                {
-                    Object.keys(data).map(key => {
-                        const skills = data[key].join(',');
+            {
+                Object.keys(data).map(key => {
+                    const skills = data[key];
 
-                        return (
-                            <li key={key}>{key}: {skills}</li>
-                        )
-                    })
-                }
-            </ul>
+                    return (
+                        <div key={key}>
+                            <h3>{key}</h3>
+                            {skillBadges(skills)}
+                        </div>
+                    )
+                })
+            }
         </section>
     )
 }
