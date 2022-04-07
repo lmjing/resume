786be4164e88ab3bfec97d5d16638c4913d229a6
lmjing <lmjing4905@gmail.com>
2022-04-07 23:38:49 +0900

    Education 구현

---
+++ src/components/Education.js
@@ -0,0 +1,15 @@
+import Title from "./Title";
+
+function Education() {
+    return (
+        <section id="education">
+            <Title text="Education"/>
+            <div className="content">
+                <span className="text-bold">2013.03-2018.08</span>
+                <span>인천대학교 컴퓨터공학부</span>
+            </div>
+        </section>
+    )
+}
+
+export default Education
